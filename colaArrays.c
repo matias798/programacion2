066bf8b025fcff46b0952309167dbd2d611fe6fc
Matias quiroga <58121395+matias798@users.noreply.github.com>
2020-10-28 19:51:03 -0300

    Add files via upload
    esta en proceso

---
+++ colaArrays.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//var
+int A[10];
+int front = -1;
+int rear =-1;
+
+
+
+//Aniade un elemento al final de la cola
+encolar(){}
+
+//Elimina y retorna el primer elemento de la cola. El segundo pasa a ser el primero luego de la operacion
+desencolar(){}
+
+//Retorna pero no elimina al primer elemento
+frente(){}
+
+//Vacia la cola, el tama˜n´o pasa a ser cero
+vaciaCola(){}
+
+//tamanio retorna el tamanio de la cola, el numero de elementos
+tamanio(){}
+
+//Retorna verdadero si esta vacia.
+estaVacia(){
+if(front == -1 && rear ==-1)
+{printf("Se encuentra vacio");}
+else
+{printf("Se encuentran elementos");
+}
+}
+
+int  main (){estaVacia();}
